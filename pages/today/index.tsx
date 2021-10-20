5ce1901ca8b0d02ebf6bdb83fb0d21daa5cf8cf6
keisuke ohashi <kog71903@gmail.com>
2021-10-20 22:03:04 +0900

    material-ui setup

--- pages/today/index.tsx
+++ pages/today/index.tsx
@@ -1,9 +1,29 @@
+import React, { useState } from 'react';
 import type { NextPage } from 'next';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Today: NextPage = () => {
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const body = event.target.body.value;
+    console.log('name is', name);
+    console.log('body is', body);
+  };
+
   return (
     <div>
       <div>today</div>
+      <form onSubmit={onSubmit}>
+        <TextField label="name" name="name" />
+
+        <TextField label="body" name="body" />
+
+        <Button type="submit" variant="contained" color="inherit">
+          Login
+        </Button>
+      </form>
     </div>
   );
 };
