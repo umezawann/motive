73df10ab035241a00bf5437dcda961df647293f4
keisuke ohashi <kog71903@gmail.com>
2021-10-21 22:53:32 +0900

    add key prop

--- components/templates/History/History.tsx
+++ components/templates/History/History.tsx
@@ -1,24 +1,31 @@
 interface Prop {
-  histories: any[]
+  histories: any[];
 }
 
-const todos = Array(140).fill(0)
-const History = ({histories}: Prop) => {
+const todos = Array(140).fill(0);
+const History = ({ histories }: Prop) => {
   return (
     <div>
       {histories.map((his, idx) => (
-        <div key={idx} >{his.name}</div>
+        <div key={idx}>{his.name}</div>
       ))}
 
-      <div style={{display: 'flex', flexWrap: 'wrap', width: 280}}>
-        {todos.map(todo => (
-          <div style={{width: 20, height: 20, display: 'inline-block', border: '1px solid #ccc', background: 'green'}}/>
-
+      <div style={{ display: 'flex', flexWrap: 'wrap', width: 280 }}>
+        {todos.map((todo, idx) => (
+          <div
+            key={idx}
+            style={{
+              width: 20,
+              height: 20,
+              display: 'inline-block',
+              border: '1px solid #ccc',
+              background: 'green',
+            }}
+          />
         ))}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default History
+export default History;
