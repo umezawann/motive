056264227e9fbac75b041bc78a1d6ac147b0c3a9
keisuke ohashi <kog71903@gmail.com>
2021-10-21 21:59:08 +0900

    create GitHub contributions like ui

--- components/templates/History/History.tsx
+++ components/templates/History/History.tsx
@@ -2,6 +2,7 @@ interface Prop {
   histories: any[]
 }
 
+const todos = Array(140).fill(0)
 const History = ({histories}: Prop) => {
   return (
     <div>
@@ -9,6 +10,12 @@ const History = ({histories}: Prop) => {
         <div key={idx} >{his.name}</div>
       ))}
 
+      <div style={{display: 'flex', flexWrap: 'wrap', width: 280}}>
+        {todos.map(todo => (
+          <div style={{width: 20, height: 20, display: 'inline-block', border: '1px solid #ccc', background: 'green'}}/>
+
+        ))}
+      </div>
 
     </div>
   )
