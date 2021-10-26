f1fee919b4c8ad181c4943b41dbdadf5e18abc43
umezawann <yaotomegaku816trigger@gmail.com>
2021-10-26 19:08:24 +0900

    途中

---
+++ web/components/templates/Color/box.tsx
@@ -0,0 +1,7 @@
+const Box = () => {
+  return (
+    <div style={{ width: "10px", height: "10px", borderRadius: "5px", backgroundColor: 'green' }}></div>
+  );
+};
+
+export default Box;
---
+++ web/components/templates/Color/color.tsx
@@ -0,0 +1,19 @@
+import Box from "./box";
+
+const Color = () => {
+  return (
+    <div>
+      <div>Less</div>
+      <div>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </div>
+      <div>More</div>
+    </div>
+  );
+};
+
+export default Color;
