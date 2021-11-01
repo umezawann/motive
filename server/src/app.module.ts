e810751c27e8cd8667f9688b4b0c2af0eff1dab7
keisuke ohashi <kog71903@gmail.com>
2021-11-01 21:12:54 +0900

    nest g module users

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, AuthModule],
+  imports: [TasksModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
