b91e787e8dd1c2eb69ec7571dcca64feab0904ec
keisuke ohashi <kog71903@gmail.com>
2021-11-01 22:11:24 +0900

    resolve error

--- web/pages/today/index.tsx
+++ web/pages/today/index.tsx
@@ -3,10 +3,8 @@ import type { NextPage } from 'next';
 import Year from '@/components/templates/Year/Year';
 import TaskForm from '@/components/templates/TaskForm/TaskForm';
 import { useHooks } from './hooks';
-import Color from '@/components/templates/Color/color';
 import BaseLayout from '@/components/templates/Layouts/BaseLayout';
 import Grid from '@mui/material/Grid';
-
 
 
 const Today: NextPage = () => {
@@ -22,7 +20,6 @@ const Today: NextPage = () => {
             <div style={{ margin: 'auto' }}>本日のタスク</div>
             <TaskForm onSubmit={onSubmit} />
             {tasks && tasks.map((t) => <div key={t.id}>{t.title}</div>)}
-      <Color />            
           </div>
         </Grid>
         <Grid item xs={2} sm={2} />
