5234b5108dc0b41408e4d6410fcbb9ec0251c5f0
keisuke ohashi <kog71903@gmail.com>
2021-11-01 19:22:18 +0900

    grid layout

--- web/pages/today/index.tsx
+++ web/pages/today/index.tsx
@@ -3,7 +3,8 @@ import type { NextPage } from 'next';
 import Year from '@/components/templates/Year/Year';
 import TaskForm from '@/components/templates/TaskForm/TaskForm';
 import { useHooks } from './hooks';
-import BaseLayout from '@/components/templates/Layouts/BaseLayout'
+import BaseLayout from '@/components/templates/Layouts/BaseLayout';
+import Grid from '@mui/material/Grid';
 
 const Today: NextPage = () => {
   const { onSubmit, tasks, tasksInYear } = useHooks();
@@ -11,14 +12,20 @@ const Today: NextPage = () => {
   console.log('tasks', tasks);
   return (
     <BaseLayout>
-      <div>本日のタスク</div>
-      <TaskForm onSubmit={onSubmit} />
-      {tasks && tasks.map(t => (
-        <div key={t.id}>{t.title}</div>
-      ))}
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item xs={2} sm={2} />
+        <Grid item xs={8} sm={8}>
+          <div>
+            <div style={{ margin: 'auto' }}>本日のタスク</div>
+            <TaskForm onSubmit={onSubmit} />
+            {tasks && tasks.map((t) => <div key={t.id}>{t.title}</div>)}
 
-      {tasksInYear && <Year tasks={tasksInYear} />}
-      </BaseLayout>
+            {tasksInYear && <Year tasks={tasksInYear} />}
+          </div>
+        </Grid>
+        <Grid item xs={2} sm={2} />
+      </Grid>
+    </BaseLayout>
   );
 };
 
