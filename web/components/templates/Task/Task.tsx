7cd7f0473904f75ec6829ed4b5f587b50e0c5f26
keisuke ohashi <kog71903@gmail.com>
2021-11-04 22:36:38 +0900

    commit

--- web/components/templates/Task/Task.tsx
+++ web/components/templates/Task/Task.tsx
@@ -4,16 +4,72 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-const Task = ({ task }) => {
+interface TaskProp {
+  task: {
+    id: string;
+  };
+}
+
+const Task = ({ task }: TaskProp) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Card>
-      <CardContent>
-        <div>{JSON.stringify(task)}</div>
-      </CardContent>
-    </Card>
+    <>
+      <Card onClick={handleClickOpen}>
+        <CardContent>
+          <div>{JSON.stringify(task)}</div>
+        </CardContent>
+      </Card>
+      <FormDialog open={open} handleClose={handleClose} task={task} />
+    </>
   );
 };
+
+interface FormDialogProp {
+  open: boolean;
+  handleClose: () => void;
+  task: {
+    id: string;
+  };
+}
+
+function FormDialog({ open, handleClose, task }: FormDialogProp) {
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Subscribe</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{task.id}</DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleClose}>Subscribe</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
 
 export default Task;
