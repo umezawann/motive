f3cd233c909058014ad6cd24144a0fee8caa2a09
keisuke ohashi <kog71903@gmail.com>
2021-11-07 22:53:40 +0900

    sortable board

--- web/components/templates/Board/Board.tsx
+++ web/components/templates/Board/Board.tsx
@@ -1,39 +1,15 @@
 import React from 'react';
-import Draggable from 'react-draggable';
+import { Container } from './Container';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const Board = () => {
-  const eventLogger = (e: MouseEvent, data: Object) => {
-    console.log('Event: ', e);
-    console.log('Data: ', data);
-  };
-
-  const handleStart = () => {
-    console.log('handleStart');
-  };
-  const handleDrag = () => {
-    console.log('handleDrag');
-  };
-  const handleStop = () => {
-    console.log('handleStop');
-  };
-
   return (
-    <Draggable
-      axis="both"
-      handle=".handle"
-      defaultPosition={{ x: 0, y: 0 }}
-      // position={null}
-      grid={[25, 25]}
-      scale={1}
-      onStart={handleStart}
-      onDrag={handleDrag}
-      onStop={handleStop}
-    >
-      <div className="handle" style={{border: '1px solid #ccc', width: 300, height: 300}}>
-        <div >Drag from here</div>
-        <div>This readme is really dragging on...</div>
-      </div>
-    </Draggable>
+    <div>
+      <DndProvider backend={HTML5Backend}>
+        <Container />
+      </DndProvider>
+    </div>
   );
 };
 
