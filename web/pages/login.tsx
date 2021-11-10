5282baec25370f7e4ce1362613b7b0b0f954948a
keisuke ohashi <kog71903@gmail.com>
2021-11-10 19:56:48 +0900

    link

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,10 +1,75 @@
-import type { NextPage } from "next";
-import AuthLayout from "@/components/templates/Layouts/AuthLayout";
+import type { NextPage } from 'next';
+import AuthLayout from '@/components/templates/Layouts/AuthLayout';
+import { Form, Field } from 'react-final-form';
+import Button, { ButtonProps } from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Link from 'next/link';
 
 const Login: NextPage = () => {
+  const onSubmit = (values) => {
+    console.log('hogehoge', values);
+  };
+
   return (
     <AuthLayout>
-    <div>login</div>
+      <div>
+        <Form
+          onSubmit={onSubmit}
+          initialValues={{}}
+          // validate={validate}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit} noValidate>
+              <div>
+                <label>username</label>
+                <Field name="username">
+                  {(props) => (
+                    <div>
+                      <TextField
+                        name={props.input.name}
+                        value={props.input.value}
+                        onChange={props.input.onChange}
+                      />
+                    </div>
+                  )}
+                </Field>
+              </div>
+              <div>
+                <label>password</label>
+                <Field name="password">
+                  {(props) => (
+                    <div>
+                      <TextField
+                        type="password"
+                        name={props.input.name}
+                        value={props.input.value}
+                        onChange={props.input.onChange}
+                      />
+                    </div>
+                  )}
+                </Field>
+              </div>
+              <div>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={submitting}
+                >
+                  Submit
+                </Button>
+              </div>
+
+            </form>
+          )}
+        />
+
+        <div>
+          アカウントをお持ちではないですか？
+          <Link href="/signup">
+            <a>サインアップ</a>
+          </Link>
+        </div>
+      </div>
     </AuthLayout>
   );
 };
--- web/pages/signup.tsx
+++ web/pages/signup.tsx
@@ -3,6 +3,7 @@ import AuthLayout from '@/components/templates/Layouts/AuthLayout';
 import { Form, Field } from 'react-final-form';
 import Button, { ButtonProps } from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Link from 'next/link';
 
 const Signup: NextPage = () => {
   const onSubmit = (values) => {
@@ -11,55 +12,64 @@ const Signup: NextPage = () => {
 
   return (
     <AuthLayout>
-      <Form
-        onSubmit={onSubmit}
-        initialValues={{}}
-        // validate={validate}
-        render={({ handleSubmit, form, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit} noValidate>
-            <div>
-              <label>username</label>
-              <Field name="username">
-                {(props) => (
-                  <div>
-                    <TextField
-                      name={props.input.name}
-                      value={props.input.value}
-                      onChange={props.input.onChange}
-                    />
-                  </div>
-                )}
-              </Field>
-            </div>
-            <div>
-              <label>password</label>
-              <Field name="password">
-                {(props) => (
-                  <div>
-                    <TextField
-                    type='password'
-                      name={props.input.name}
-                      value={props.input.value}
-                      onChange={props.input.onChange}
-                    />
-                  </div>
-                )}
-              </Field>
-            </div>
-            <div>
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                disabled={submitting}
-              >
-                Submit
-              </Button>
-            </div>
-            <pre>{JSON.stringify(values, 0, 2)}</pre>
-          </form>
-        )}
-      />
+      <div>
+        <Form
+          onSubmit={onSubmit}
+          initialValues={{}}
+          // validate={validate}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit} noValidate>
+              <div>
+                <label>username</label>
+                <Field name="username">
+                  {(props) => (
+                    <div>
+                      <TextField
+                        name={props.input.name}
+                        value={props.input.value}
+                        onChange={props.input.onChange}
+                      />
+                    </div>
+                  )}
+                </Field>
+              </div>
+              <div>
+                <label>password</label>
+                <Field name="password">
+                  {(props) => (
+                    <div>
+                      <TextField
+                        type="password"
+                        name={props.input.name}
+                        value={props.input.value}
+                        onChange={props.input.onChange}
+                      />
+                    </div>
+                  )}
+                </Field>
+              </div>
+              <div>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={submitting}
+                >
+                  Submit
+                </Button>
+              </div>
+            </form>
+          )}
+        />
+
+        <div>
+          すでにサインアップ済みですか？
+          <Link href="/login">
+            <a>ログイン</a>
+          </Link>
+          に移動する
+        </div>
+      </div>
     </AuthLayout>
   );
 };
