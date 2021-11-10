e2639299869e627365d0d1dfd8b41da248bad068
keisuke ohashi <kog71903@gmail.com>
2021-11-10 20:03:16 +0900

    submit functions

--- web/pages/signup/hooks.ts
+++ web/pages/signup/hooks.ts
@@ -2,12 +2,12 @@ import axios from '@/lib/axios';
 import { useTasksOfDay, useTasksInYear } from '@/lib/api/tasks';
 
 export const useHooks = () => {
-  const onSubmit = async (values: any) => {
-    console.log('values', values);
+  const onSubmit = async (body: any) => {
+    console.log('body', body);
     // const body = { ...values, point: 1, status: 'TODO', date: new Date() };
     // console.log('body is', body);
-    // const res = await axios.post('/tasks', body);
-    // console.log('res is', res);
+    const res = await axios.post('/auth/signup', body);
+    console.log('res is', res);
   };
 
   return { onSubmit};
