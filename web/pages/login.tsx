2c71f2a77cb63bbec69a1c7b0a4bdde998ff23bb
keisuke ohashi <kog71903@gmail.com>
2021-11-10 19:35:53 +0900

    login/signup page

---
+++ web/pages/login.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import BaseLayout from "@/components/templates/Layouts/BaseLayout";
+
+const Login: NextPage = () => {
+  return (
+    <BaseLayout>
+    <div>login</div>
+    </BaseLayout>
+  );
+};
+
+export default Login;
---
+++ web/pages/signup.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import BaseLayout from "@/components/templates/Layouts/BaseLayout";
+
+const Signup: NextPage = () => {
+  return (
+    <BaseLayout>
+    <div>hogehoge</div>
+    </BaseLayout>
+  );
+};
+
+export default Signup;
