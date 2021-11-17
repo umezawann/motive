3eac66cd09a9fd83ae79f16506e2f90888532ab5
umezawann <yaotomegaku816trigger@gmail.com>
2021-11-17 17:08:27 +0900

    finish

--- web/pages/login/hooks.tsx
+++ web/pages/login/hooks.tsx
@@ -2,13 +2,17 @@ import axios from '@/lib/axios';
 import { useTasksOfDay, useTasksInYear } from '@/lib/api/tasks';
 import { get, set } from '@/lib/storage'
 import { apiClient } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 export const useHooks = () => {
+  const router = useRouter()
+
   const onSubmit = async (body: any) => {
     const res = await apiClient.post('/auth/login', body);
     const data: any = res.data
     const accessToken = data.access_token
     set('accessToken', accessToken)
+    router.push('/')
   };
 
   return { onSubmit};
