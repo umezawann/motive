5656cabb916161bb1212e963e3dbb00a2a4fa395
umezawann <yaotomegaku816trigger@gmail.com>
2021-11-24 19:27:40 +0900

    route.queryが取得できない

--- web/pages/labels/[id].tsx
+++ web/pages/labels/[id].tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next';
 import BaseLayout from '@/components/templates/Layouts/BaseLayout'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 const Labels: NextPage = () => {
-  const router = useRouter()
-  console.log('router is', router)
-  console.log('router.query is', router.query)
+  const router = useRouter();
+  console.log('router', router)
+  console.log('router.query', router.query)
+  // console.log('router', router)
   const { pid } = router.query
 
   return (
