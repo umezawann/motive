e1e003882e83ffccc69f7ba412c3a445a70193dc
umezawann <yaotomegaku816trigger@gmail.com>
2021-11-25 19:25:26 +0900

    idでのGETができない

--- web/pages/labels/[id]/index.tsx
+++ web/pages/labels/[id]/index.tsx
@@ -1,27 +1,39 @@
-import type { NextPage } from 'next';
-import BaseLayout from '@/components/templates/Layouts/BaseLayout'
-import { useRouter } from 'next/router';
-import { useApiClient } from '@/lib/api/apiClient';
-
+import type { NextPage } from "next";
+import BaseLayout from "@/components/templates/Layouts/BaseLayout";
+import { useRouter } from "next/router";
+import { useApiClient } from "@/lib/api/apiClient";
+import Task from "@/components/templates/Task/Task";
 
 const Labels: NextPage = () => {
   const router = useRouter();
-  console.log('router', router)
-  console.log('router.query', router.query)
-  const { id } = router.query
-  const useTasksInYear = () => {
+  console.log("router", router);
+  console.log("router.query", router.query);
+  const { id } = router.query;
+
+  const useTasksInYear = (id) => {
     const { response, loading, error } = useApiClient({
-      method: 'GET',
-      url: '/tasks',
+      method: "GET",
+      url: `/labels/${id}`,
     });
-  
+
     return { data: response, loading, error };
   };
 
+  const { data: tasksInLabel } = useTasksInYear("ckw68qwc300021uvbqfeg3e2z");
+  
+  if (!id) return null;
+
+  // if(!id) {return null}
 
   return (
     <BaseLayout>
       Post: {id}
+      {tasksInLabel &&
+        tasksInLabel.map((t) => (
+          <div key={t.id}>
+            <Task task={t.task} />
+          </div>
+        ))}
     </BaseLayout>
   );
 };
